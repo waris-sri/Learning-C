49844d0e4d62e52c7d0b88b46d66fc38d3f96416
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2024-08-21 22:06:17 +0700

    Update LeapYear.c

--- Self/LeapYear.c
+++ Self/LeapYear.c
@@ -2,21 +2,18 @@
 
 int main()
 {
-  int year;
-  printf("Enter a year: ");
-  scanf("%d", &year);
-  // Leap years are divisible by 4 and 400, but not 100.
-  if (year % 4 == 0)
-  {
-    printf("%d is a leap year", year);
-  }
-  else if (year % 400 == 0)
-  {
-    printf("%d is a leap year", year);
-  }
-  else
-  {
-    printf("%d is not a leap year", year);
-  }
-  return 0;
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Leap years are divisible by 4, but not by 100, unless also divisible by 400.
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+    {
+        printf("%d is a leap year", year);
+    }
+    else
+    {
+        printf("%d is not a leap year", year);
+    }
+    return 0;
 }
