3520e0a361a7014afa8f61553e5ef96eaf9e81c6
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-09-07 12:47:05 +0700

    Done

--- Labs/671-lab5-q4-waris-sri/main.c
+++ Labs/671-lab5-q4-waris-sri/main.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 
+// Implement "Sieve of Eratosthenes"
 int main() {
   int n;
+
   do {
     scanf("%d", &n);
-    for (int i = 2; i <= n; i++) {
-      for (int j = 2; j <= n; j++) {
-        if (i != j && i % j == 0) {
-          break;
-        }
-        if (j == n) {
-          printf("%d ", i);
-        }
+  } while (n < 2 || n > 1000);
+
+  // Assume all numbers are prime
+  // 1 = prime (true), 0 = not prime (false)
+  int primes[n];
+  for (int i = 0; i <= n; i++) {
+    primes[i] = 1;
+  }
+
+  // Sieve: Mark all multiples of m as 0 iff primes[m] is 1
+  // The remains are prime numbers (nor marked)
+  // Initialize m as 2 because 0 and 1 aren't primes'
+  for (int m = 2; m * m <= n; m++) {
+    if (primes[m] == 1) {
+      for (int i = m * m; i <= n; i += m) {
+        primes[i] = 0;
       }
     }
-    break;
-  } while (n >= 2 || n <= 1000);
+  }
+
+  for (int i = 2; i <= n; i++) {
+    if (primes[i] == 1) {
+      printf("%d ", i);
+    }
+  }
 }
