43b6adf909fe5bc81d2b5cad7447e3eb4344e569
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-09-19 21:06:59 +0700

    +1

--- Self/labScores.c
+++ Self/labScores.c
@@ -1,39 +1,68 @@
 #include <stdio.h>
 
 int main(void) {
-  int scores[5][3], sum = 0;
+  float scores[5][3];
 
+  // input prompts
   for (int i = 0; i < 5; i++) {
     printf("Student %d:\n", i + 1);
     for (int j = 0; j < 3; j++) {
       printf("Lab %d: ", j + 1);
-      scanf("%d", &scores[i][j]);
+      scanf("%f", &scores[i][j]);
     }
     printf("\n");
   }
 
+  printf("Results:\n");
+
   // average of all students and labs
+  float sumAll = 0;
   for (int i = 0; i < 5; i++) {
     for (int j = 0; j < 3; j++) {
-      sum += scores[i][j];
+      sumAll += scores[i][j];
     }
   }
-  printf("Average of Scores: %d\n", sum / 15);
+  printf("Average Score: %.2f\n", sumAll / 15);
 
-  // max and min scores of all students and labs, and who got them
-  int min = scores[0][0], max = scores[0][0];
+  // max and min scores of all students from all labs
+  float min = scores[0][0], max = scores[0][0];
   for (int i = 0; i < 5; i++) {
     for (int j = 1; j < 3; j++) {
       if (scores[i][j] < min) {
         min = scores[i][j];
       }
       if (scores[i][j] > max) {
-        // printf("yes");
         max = scores[i][j];
       }
     }
   }
-  printf("Max Score: %d\nMin Score: %d", max, min);
-  // FIXME: max/min scores as in all labs, 1 student
+  printf("Global Maximum: %.2f\nGlobal Minimum: %.2f\n", max, min);
+
+  // max and min scores from all labs, 1 student
+  float sumRow[5] = {0};
+  float sumEach = 0;
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 3; j++) {
+      sumRow[i] += scores[i][j];
+    }
+    sumEach += sumRow[i];
+  }
+  float minStudent = sumRow[0], maxStudent = sumRow[0];
+  int minStudentIndex, maxStudentIndex;
+  for (int i = 0; i < 5; i++) {
+    if (sumRow[i] < minStudent) {
+      minStudent = sumRow[i];
+      minStudentIndex = i;
+    }
+    if (sumRow[i] > maxStudent) {
+      maxStudent = sumRow[i];
+      maxStudentIndex = i;
+    }
+  }
+  printf("Student %d scored the highest (%.2f).\n", maxStudentIndex + 1,
+         maxStudent);
+  printf("Student %d scored the lowest (%.2f).", minStudentIndex + 1,
+         minStudent);
+
   return 0;
 }
