7b37d0eac8466a34ef913e021bfd991f945caf48
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2024-10-17 17:01:16 +0700

    main.c の更新

--- Labs/lab09/671-lab9-q3-waris-sri/main.c
+++ Labs/lab09/671-lab9-q3-waris-sri/main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+void print_c(int n, char ch) {
+  for (int i = 0; i < n; i++) {
+    printf("%c", ch);
+  }
+}
+
+int main() {
+  int n, m;
+  char ch;
+  scanf("%d %d %c", &n, &m, &ch);
+  for (int i = 0; i < n; i++) {
+      print_c(m, ch);
+      printf("\n");
+  }
+  return 0;
+}
