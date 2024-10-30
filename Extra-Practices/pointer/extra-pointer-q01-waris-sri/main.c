3cb6f5826f5c7e305ed40c2e1ed3863fa62b11dc
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-10-31 02:11:08 +0700

    Done

--- Extra-Practices/pointer/extra-pointer-q01-waris-sri/main.c
+++ Extra-Practices/pointer/extra-pointer-q01-waris-sri/main.c
@@ -15,13 +15,21 @@ int n;
 void sort(int *array) {
   // Your code started here.
   for (int i = 1; i < n; i++) {
-    int current = *(array + i); // pointer-array offset
+    // pointer-array offset
+    int current = *(array + i); // is `array[i]`
     // the last sorted number is right behind the current index `i`
     int sorted_idx = i - 1;
+    // using pointer-array offset,
+    // while value at `sorted_idx` is larger than `current`,
     while (sorted_idx >= 0 && *(array + sorted_idx) > current) {
+      // shift it forward by one spot to make space for `current` to insert
+      // `array[sorted_idx + 1] = array[sorted_idx]`
       *(array + sorted_idx + 1) = *(array + sorted_idx);
+      // move to the previous element in the sorted portion
       sorted_idx = sorted_idx - 1;
     }
+    // insert 'current' into its correct position within the sorted numbers
+    // `array[sorted_idx + 1] = current`
     *(array + sorted_idx + 1) = current;
   }
   for (int i = 0; i < n; i++) {
