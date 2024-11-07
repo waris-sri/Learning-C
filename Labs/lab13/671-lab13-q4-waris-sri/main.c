f4872ab497448177a1bf5e1471d3bb395faae147
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-11-07 16:41:59 +0700

    Edited lab13 q4

--- Labs/lab13/671-lab13-q4-waris-sri/main.c
+++ Labs/lab13/671-lab13-q4-waris-sri/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-#define N 10
+#define MAX_LEN 10
 
 void swap(char *x, char *y) {
-  char temp[N];
-  strcpy(temp, x);
-  strcpy(x, y);
-  strcpy(y, temp);
+  char temp[MAX_LEN]; // temp buffer to hold string during swap
+  strcpy(temp, x);    // save x string to temp
+  strcpy(x, y);       // copy y into x
+  strcpy(y, temp);    // copy original x from temp into y
 }
 
 int main(void) {
@@ -15,12 +15,15 @@ int main(void) {
   scanf("%s", s1);
   scanf("%s", s2);
   scanf("%s", s3);
+  // if the 1st unmatched char is greater in s1 than s2
   if (strcmp(s1, s2) > 0) {
     swap(s1, s2);
   }
   if (strcmp(s2, s3) > 0) {
     swap(s2, s3);
   }
+  // after swapping s2 and s3, s2's new value could now be less than s1
+  // so we need to check s1 vs s2 one more time to ensure proper ordering
   if (strcmp(s1, s2) > 0) {
     swap(s1, s2);
   }
