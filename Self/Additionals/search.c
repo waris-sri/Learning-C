c07bb11f3d94cf846d81bede9656310607a3c1ad
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-12-03 23:05:42 +0700

    Removed unnecessary codes and edited comments

--- Self/Additionals/search.c
+++ Self/Additionals/search.c
@@ -11,37 +11,30 @@ struct Student {
 };
 
 int search(char *prefix, char *out, struct Student students[], int size) {
-  char temp[MAX_LENGTH];
-  char *pos;
-  int found = 0;
+  char temp[MAX_LENGTH]; // For holding matching strings
+  int found = 0;         // Assume as not found first
   for (int i = 0; i < size; i++) {
     int k = 0; // Keep track of characters to add to suggestion
 
-    // For student name suggestions
+    /* For student name suggestions */
     // `strncasecmp` makes the checking process case-insensitive
     if (strncasecmp(students[i].name, prefix, strlen(prefix)) == 0) {
       for (int j = strlen(prefix); j < strlen(students[i].name); j++) {
         temp[k] = students[i].name[j];
         k++;
       }
-      temp[k] = '\0';
-      pos = strchr(temp, '\n');
-      if (pos != NULL)
-        *pos = '\0';
+      temp[k] = '\0'; // Terminate the `temp` string
       printf("%s\033[30;44m[%s]\033[0m\n", prefix, temp);
       found = 1;
     }
 
-    // For student ID suggestions
+    /* For student ID suggestions */
     if (strncmp(students[i].id, prefix, strlen(prefix)) == 0) {
       for (int j = strlen(prefix); j < strlen(students[i].id); j++) {
         temp[k] = students[i].id[j];
         k++;
       }
-      temp[k] = '\0';
-      pos = strchr(temp, '\n');
-      if (pos != NULL)
-        *pos = '\0';
+      temp[k] = '\0'; // Terminate the `temp` string
       printf("%s\033[30;44m[%s]\033[0m\n", prefix, temp);
       found = 1;
     }
@@ -59,13 +52,8 @@ int main() {
       {"copter", "109"}, {"bhone", "999"}};
   char suggestion[MAX_LENGTH], line[MAX_LENGTH];
   scanf("%s", line);
-  char *pos;
-  pos = strchr(line, '\n');
-  if (pos != NULL) {
-    *pos = '\0';
-  }
-  int found = search(line, suggestion, students, SIZE);
-  if (!found) {
+  int is_match = search(line, suggestion, students, SIZE);
+  if (!is_match) {
     printf("\033[1;30;31mNot found!\033[0m\n");
   }
   return 0;
